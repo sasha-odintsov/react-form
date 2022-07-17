383912ff4c55e9a658deb888900fb8b56f9ae4af
Sasha <aleks-one@ukr.net>
2022-07-17 16:10:43 +0300

    Added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const loginCorrect = 'artem@ithillel.ua';
+  const passwordCorrect = '123456';
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [showAlert, setShowAlert] = useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (login !== loginCorrect) {
+      setShowAlert('Wrong login')
+    }  else if (password !== passwordCorrect) {
+      setShowAlert('Wrong password')
+    } else {
+      setShowAlert('')
+      console.log(`Welcome! Login: ${login} Password: ${password}`)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrap'>
+      <form className="form" onSubmit={handleSubmit}>
+        <p className="form-title">Log in</p>
+        <div className="form-floating">
+          <input type="email"  value={login} onChange={(event)=>{setLogin(event.target.value)}} className="form-control" id="floatingInput" placeholder="Email" />
+          <label htmlFor="floatingInput">Email</label>
+        </div>
+        <div className="form-floating">
+          <input type="password" value={password} onChange={(event)=>{setPassword(event.target.value)}} className="form-control" id="floatingPassword" placeholder="Password" />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+        <p>{showAlert}</p>
+        <button type="submit" className="btn btn-outline-primary" disabled={!login || !password}>Submit</button>
+      </form>
     </div>
   );
 }
